dd5150699bd892d955bea03ae18aa8a44ac0b951
Mel Andrade-Muñoz <xvoqt@unc.edu>
2024-05-18 23:17:22 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tinder
+//
+//  Created by Mel Andrade-Muñoz on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
